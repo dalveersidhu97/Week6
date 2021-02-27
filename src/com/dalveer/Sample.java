06c47463a6b898fa7e3041ec4599d796b0e74a76
dalveersidhu97 <dalveersidhu97@gmail.com>
2021-02-27 10:32:02 -0500

    Student Class little implementation

--- src/com/dalveer/Sample.java
+++ src/com/dalveer/Sample.java
@@ -1,8 +1,18 @@
 package com.dalveer;
 
-public class Sample {
-    public static void main(String [] args){
-        System.out.println("hellow");
+import java.util.Date;
+
+public class Sample
+{
+    public static void main(String[] args) {
+        float[] dalveerMarks = {80,80,80,80,80};
+        Student dalveer = new Student(789821, "Dalveer", "Singh", new Date(), Gender.MALE, "String address", "String city", "String pinCode", "String cellPhone", "String email", "String department", dalveerMarks);
+        dalveer.calculateGrade();
+        float[] dwarkeshMarks = {90,90,90,90,90};
+        Student dwarkesh = new Student(789822, "Dalveer", "Singh", new Date(), Gender.MALE, "String address", "String city", "String pinCode", "String cellPhone", "String email", "String department", dwarkeshMarks);
+        dalveer.calculateGrade();
+        dwarkesh.calculateGrade();
+        System.out.println("Dalveer Grades: "+dalveer.getGrade());
+        System.out.println("Dwarkesh Grades: "+dwarkesh.getGrade());
     }
 }
-
